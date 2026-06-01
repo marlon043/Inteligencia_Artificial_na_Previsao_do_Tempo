c3765a3534eed0e628bf4eb48c20f661d7d9d7b2
marlon043 <marlon.santos07@escola.pr.gov.br>
2026-06-01 11:06:17 -0300

    s

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
