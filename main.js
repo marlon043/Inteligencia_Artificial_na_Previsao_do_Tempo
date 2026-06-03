5b472dd34881e5d646908d4b36e42c27ecdfc783
marlon043 <marlon.santos07@escola.pr.gov.br>
2026-06-03 08:59:05 -0300

    commit

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+// Abre e fecha o menu ao clicar nas três barras
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
 
+// Fecha o menu automaticamente quando clica em qualquer link interno
 document.querySelectorAll(".nav-menu a").forEach(link => {
     link.addEventListener("click", () => {
         navMenu.classList.remove("active");
